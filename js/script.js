4006d69d7fad8c27ae2b34a7b141517594c2c27b
victorzlldev <joaovictor12200capa@gmail.com>
2023-08-10 17:05:38 -0300

    Nova função, arrow right
    passar de momento usando a seta direita

--- js/script.js
+++ js/script.js
@@ -1,35 +1,52 @@
-const btnDarkLight = document.querySelector('.dark-light');
-// DARK MODE
-btnDarkLight.addEventListener('click', async ()=>{
-    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: darkLightMode
-    });
-});
+// Função para alternar entre os modos escuro e claro
+function toggleDarkLightMode() {
+    const iframeContent = document.querySelector('iframe').contentDocument;
+    const fundo = iframeContent.querySelector('.reveal');
+    
+    fundo.style.transition = 'background 1s';
 
-function darkLightMode(){
-    const fundo = document.querySelector('iframe').contentDocument.querySelector('.reveal');
-    fundo.style.transition = 'background 1s'
-    if ( fundo.style.background == '#141414' || fundo.style.background == 'rgb(20, 20, 20)' ) {
+    if (fundo.style.background === '#141414' || fundo.style.background === 'rgb(20, 20, 20)') {
         fundo.style.background = '#FFFFFF';
-    }
-    else{ 
+    } else { 
         fundo.style.background = '#141414'; 
     }
-};
+}
 
-// PROXIMO MOMENTO
-const btnPM = document.querySelector('.PM');
-btnPM.addEventListener('click', async ()=>{
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+// Função para avançar para o próximo momento
+function goToNextMoment() {
+    const iframeContent = document.querySelector('iframe').contentDocument;
+    const btnIframe = iframeContent.querySelector('.btn-avanca-momento');
+    
+    btnIframe.click();
+}
+
+// Adicionar evento de clique para alternar entre modos escuro e claro
+const btnDarkLight = document.querySelector('.dark-light');
+btnDarkLight.addEventListener('click', async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: proximoMomento
+        function: toggleDarkLightMode
     });
 });
 
-function proximoMomento(){
-    const btnIframe = document.querySelector('iframe').contentDocument.querySelector('.btn-avanca-momento');
-    btnIframe.click();
-}
+// Adicionar evento de clique para avançar para o próximo momento
+const btnPM = document.querySelector('.PM');
+btnPM.addEventListener('click', async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: goToNextMoment
+    });
+});
+
+// Avançar para o próximo momento com seta direita
+addEventListener("keydown", async (e) => {
+  if (e.key == "ArrowRight") {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: goToNextMoment
+    })
+  }
+});
